d1d7c2d92ca829b2edd2f11554e5c5c4cf60fcc4
majdibo <majdi.ben.ouaghrem@gmail.com>
2020-06-03 19:38:42 +0200

    add generic form

--- src/app/base/form/shared/utils.ts
+++ src/app/base/form/shared/utils.ts
@@ -1,12 +1,10 @@
 import {NG_VALUE_ACCESSOR} from '@angular/forms';
 import {forwardRef, Provider} from '@angular/core';
 
-export function subForm(component): Provider[] {
-  return [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => component),
-      multi: true,
-    }
-  ];
+export function subForm(component): Provider {
+  return {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => component),
+    multi: true
+  };
 }
